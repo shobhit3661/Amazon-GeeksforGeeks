4b83e054145bf3625f47f62b3b8cc36e23f90cf1
shobhit sharma <shobhitsharma.3661@gmail.com>
2020-11-06 20:14:22 +0530

    06/11/2020

---
+++ InterviewQestion.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define fast  ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+typedef long long int ll;
+
+using namespace std;
+
+vector<string> getout(string a)
+{
+	vector<string> ans;
+	
+	
+	return ans;
+}
+
+void solve()
+{
+	string a;
+	cin>>a;
+	
+	vector<string> ans;
+	ans = getout(a);
+	
+	for(auto i : ans)
+		cout<<i;
+}
+
+int main()
+{
+	fast;
+	ll t=1;
+	//cin>>t;
+	while(t--)
+		solve();
+	return 0;
+}
+
+
+
