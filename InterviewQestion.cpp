5e0403504088d281c3c194e634979bddd23401ea
shobhit sharma <shobhitsharma.3661@gmail.com>
2020-11-07 21:34:37 +0530

    07/11/20202

--- InterviewQestion.cpp
+++ InterviewQestion.cpp
@@ -1,3 +1,20 @@
+/*Adarsh interview Question in Amazon
+
+Question 1:- You are given a string of numbers , You have to return a partitioned String Array in which substring (i-1)th +(i-2)th = ith substring. 
+If not possible return empty string array.
+For example:
+Input:                  Output:
+"111122335"   ------>   {"1","11","12","23","35"}  
+Input:                  Output:
+"112233"      ------->  {"11","22","33"}
+Input:                  Output:
+"11314"       ------->  {"11","3","14"} or {"1","13","14"}
+Input:                  Output:
+"13234113"     -------->{}
+
+
+*/
+
 #include<bits/stdc++.h>
 #define fast  ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
 typedef long long int ll;
