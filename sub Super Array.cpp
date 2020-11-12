da989d577349ce84a7c628c1154793691feb3b5a
shobhit sharma <shobhitsharma.3661@gmail.com>
2020-11-12 23:05:50 +0530

    11/12/2020

---
+++ sub Super Array.cpp
@@ -0,0 +1,48 @@
+/*
+	Sub SuperArray HackerRank Problem
+	
+	You are given an array  of size  containing distinct integers. You have to choose two non-overlapping subarrays and join them to make a super array such that all elements in the super array are strictly in increasing order. Find the maximum size of the super array.
+
+	For example, if the given array is  and you choose subarrays  and  to form the super array, then the super array will be  where ().
+	
+	Notes
+
+	The swapping of elements is not allowed.
+	The subarray in the final optimal answer will always contain distinct numbers.
+	It is always possible to choose 2 subarrays that follow the given criteria.
+	
+	Input:
+		1
+		10
+		7 1 2 4 6 5 3 8 9 10
+	output:
+		7
+	
+	
+	Explanation
+    For the given array if we choose a sub array from index [1,2,4,6]  and index [8,9,10] to make the super array,
+	then we get the maximum size i.e 7.  
+*/
+
+#include<bits/stdc++.h>
+#define fast  ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+typedef long long int ll;
+
+using namespace std;
+
+void solve()
+{
+	
+}
+
+int main()
+{
+	fast;
+	ll t=1;
+	while(t--)
+		solve();
+	return 0;
+}
+
+
+
